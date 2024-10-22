2fbd1e1e5b88348d4a371593a152fcba3fe72899
Jan Górecki <jan.gorecki@shopsys.com>
2024-10-23 00:41:03 +0200

    create skeleton for crud controllers

--- packages/administration/src/ShopsysAdministrationBundle.php
+++ packages/administration/src/ShopsysAdministrationBundle.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace Shopsys\AdministrationBundle;
 
+use Shopsys\AdministrationBundle\DependencyInjection\Compiler\InicializeControllersCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ShopsysAdministrationBundle extends Bundle
 {
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new InicializeControllersCompilerPass());
+    }
 }
